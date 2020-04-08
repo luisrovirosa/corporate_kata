22e45776f543bb82223e52cdf6fe534bd55504f2
Luis Rovirosa <luis@codium.team>
2020-04-08 10:16:13 +0200

    test skeleton

---
+++ tests/ATest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Katas\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ATest extends TestCase
+{
+    /** @test */
+    public function xxx()
+    {
+        $this->assertEquals("", "");
+    }
+}
